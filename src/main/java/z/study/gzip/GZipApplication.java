e9c1ee03a39b6348b17a6f9dd5233f7ac847dba3
River <20207075@qq.com>
2020-05-01 22:53:09 +0800

    init

---
+++ src/main/java/z/study/gzip/GZipApplication.java
@@ -0,0 +1,13 @@
+package z.study.gzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GZipApplication.class, args);
+    }
+
+}
---
+++ src/main/java/z/study/gzip/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package z.study.gzip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: RiverDemo
+ * @description:
+ * @author: River
+ * @create: 2020-05-01 11:14
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
